af86937fbc1c2f339447ebab5dd35473adae809f
Nathan Dallmann <69879357+Bing-hash@users.noreply.github.com>
2025-07-24 22:35:42 -0400

    Create 3dMine.js
    Main game file. Both canvases have been combined at this point. This update contains work for the last year. I have stored the code in the P5.js web editor for convenience purposes.
    
    To-do:
    - Game lags on every mouse click. I would assume it has to do with looping through every cell. With the new detection system, we can get exactly what column and row using pixel math. We don't even really need the loop anymore. Removed the loops!

---
+++ Cannonball/3dMine.js
@@ -0,0 +1,51 @@
+class StartMine{
+  constructor(sphereRadius, sphereDetailX, sphereDetailY, cylRadius, cylHeight, cylDetailX, cylDetailY, cylBottomCap, cylTopCap, spinSpeed) {
+    this.sphereRadius = sphereRadius;
+    this.sphereDetailX = sphereDetailX;
+    this.sphereDetailY = sphereDetailY;
+    
+    this.cylRadius = cylRadius;
+    this.cylHeight = cylHeight;
+    this.cylDetailX = cylDetailX;
+    this.cylDetailY = cylDetailY;
+    this.cylBottomCap = cylBottomCap;
+    this.cylTopCap = cylTopCap;
+    
+    this.spinSpeed = spinSpeed;
+    
+    
+    // Mine model
+    beginGeometry();
+    sphere(this.sphereRadius, this.sphereDetailX, this.sphereDetailY);
+    this.mineModel = endGeometry();
+
+
+    // Hertz horns model
+    beginGeometry();
+    cylinder(this.cylRadius, this.cylHeight, this.cylDetailX, this.cylDetailY, this.cylBottomCap, this.cylTopCap);
+    this.hornModel = endGeometry();
+      
+  }
+  
+  display(mineScale) {
+    // Drawing mine
+    push();
+    scale(mineScale);
+    model(this.mineModel);
+    pop();
+    // Drawing Hertz horns
+    model(this.hornModel);
+
+    for(let i=0; i<8; i++) {
+      rotateZ(PI/4);
+      model(this.hornModel);
+      rotateZ(-PI/4);
+      rotateY(PI/4);
+    }
+    
+  }
+  
+  mouseOver() {
+    
+  }
+}
